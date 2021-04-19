52d81fb7d7425e559a32716bdcd544e7b0bc6a08
Tariq Ebadi <tariqstock@gmail.com>
2021-04-19 09:06:55 -0700

    updated gradle, updated manifest, fixed issues and many headaches

--- app/src/main/java/com/example/musicplayer/data/entities/Song.kt
+++ app/src/main/java/com/example/musicplayer/data/entities/Song.kt
@@ -3,9 +3,9 @@ package com.example.musicplayer.data.entities
 //make data class inside entities package to hold songs
 //Auto Parsed when we grab from firestore
 data class Song (
-    val artist:String = "",
     val mediaID:String = "",
     val title:String = "",
+    val artist:String = "",
     val songURL:String = "",
     val img:String = ""
     )
--- app/src/main/java/com/example/musicplayer/ui/MainActivity.kt
+++ app/src/main/java/com/example/musicplayer/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.musicplayer.ui.viewmodels
+package com.example.musicplayer.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
